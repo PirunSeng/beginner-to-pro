755dd3e7fb8a5be210fabd37c3d0dae090705e49
PirunSeng <sengpirun.rain@gmail.com>
2020-01-07 09:42:09 +0700

    directives and custom directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,52 @@ export class AppComponent {
     likesCount: 10
   };
 
+  courses = [
+    { id: 1, name: 'Angular' },
+    { id: 2, name: 'JavaScript' },
+    { id: 3, name: 'TypeScript' }
+  ];
+  viewMode = 'default';
+
+  otherCourses;
+
+  canSave = true;
+
+  task = {
+    title: 'Review',
+    assignee: null
+  };
+
   onFavoriteChanged(eventArgs: FavoriteChangeEventArgs) {
     console.log('favorite changed...: ', eventArgs);
   }
 
   onLikeChanged(eventArgs: LikeChangeEventArgs) {
     this.tweet.isLiked = eventArgs.newValue;
+  }
+
+  onAddCourse() {
+    this.courses.push({ id: 4, name: 'NodeJS' });
+  }
+
+  onRemoveCourse(course: any) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  onUpdateCourse(course: any) {
+    course.name = 'Updated';
+  }
+
+  onLoadOtherCourse() {
+    this.otherCourses = [
+      { id: 1, name: 'HTML' },
+      { id: 2, name: 'CSS' },
+      { id: 3, name: 'JavaScript' }
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { TitleCasePipe } from './title-case.pipe';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { LikeComponent } from './like/like.component';
     FavoriteComponent,
     TitleCasePipe,
     PanelComponent,
-    LikeComponent
+    LikeComponent,
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
